18c752cadfc6cee176d14fb4806ad26043c9adb7
logon84 <logon84@users.noreply.github.com>
2021-11-19 01:57:02 +0100

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+if [ "$(id -u)" = "0" ]; then
+    echo "This script must be run as normal user" 1>&2
+    exit 1
+else
+
+APP="/usr/share/spotify"
+#--force-device-scale-factor=1.2
+ 
+if ps ax | grep -v grep | grep $APP | grep renderer > /dev/null
+    then
+        echo "$APP application running, everything is fine" 1>&2
+    else
+        while ! ps ax | grep -v grep | grep $APP | grep renderer > /dev/null
+        do
+            killall spotify
+            env LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify &
+            sleep 2
+        done
+        kdocker -o -n Spotify -d 5 -i /usr/share/icons/hicolor/512x512/apps/spotify-client.png -q 
+    fi
+fi
